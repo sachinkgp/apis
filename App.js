362cbce199828a24fc6fb4bb0ef6a363f52b55f6
Sachin Singh <sachinsinghiitkgp@gmail.com>
2022-03-22 01:34:49 +0530

    first test api

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 const express = require('express')();
-const app = express();
+const app = express;
 const PORT = 8080;
 
-app.use(express.json());
+// app.use(express.json());
 
 app.get('/testuri1',(req,res)=>{
-    res.send
+    res.status(200).send({
+        name : "sachin",
+        age : 25
+    })
 })
+
+app.listen(
+    PORT,()=>{
+        console.log(`yippi running the api at http://localhost:${PORT}`)
+    }
+)
